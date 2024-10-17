2ea13eeca9b312266a26af1ac09020ae08ca824b
kais Alkotamy <kais.alkotamy@gmail.com>
2024-10-18 02:54:30 +0400

    push updates

--- src/main/java/spring/task/consumer/config/Configuration.java
+++ src/main/java/spring/task/consumer/config/Configuration.java
@@ -6,6 +6,13 @@ import org.springframework.web.client.RestTemplate;
 @org.springframework.context.annotation.Configuration
 public class Configuration {
 
+    /**
+     * restTemplate could be configured for different type of message for ex by edit the 'messageConverters'
+     * or adding an interceptors
+     * it supports wide variety of configuration
+     * for the sake on simplicity we used the default config
+     * @return
+     */
     @Bean
     RestTemplate getRestTemplate(){
         return new RestTemplate();
